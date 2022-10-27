bc6378902a520387834ba92fb67114ca21df23c6
Virgil Dubois <tropheecommerce@gmail.com>
2022-10-27 11:44:01 +0200

    Sign-in + Firebase support

---
+++ src/components/routes/sign-in/sign-in.component.jsx
@@ -0,0 +1,22 @@
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+} from '../../../utils/firebase/firebase.utils';
+
+const { Fragment } = require('react/cjs/react.production.min');
+
+const SignIn = () => {
+  const logGoogleUser = async () => {
+    const { user } = await signInWithGooglePopup();
+    createUserDocumentFromAuth(user);
+  };
+
+  return (
+    <Fragment>
+      <h1>Sign-in page</h1>
+      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+    </Fragment>
+  );
+};
+
+export default SignIn;
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,46 @@
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyChucJLhy9fMb0ozvW-LOnDgaDkBSsiLb0',
+  authDomain: 'crwn-clothing-vd.firebaseapp.com',
+  projectId: 'crwn-clothing-vd',
+  storageBucket: 'crwn-clothing-vd.appspot.com',
+  messagingSenderId: '796418996075',
+  appId: '1:796418996075:web:e72335ddd81eda0651be2e',
+};
+
+const app = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: 'select_account',
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, { displayName, email, createdAt });
+    } catch (error) {
+      console.log('Error creating the user: ', error.message);
+    }
+  }
+};
